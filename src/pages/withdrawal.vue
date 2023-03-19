7bbebb97c94c164382ea1009d469bf21ee1b9367
lucaspouchot <lucas.pouchot@espridigital.fr>
2023-03-19 14:20:33 +0100

    use translation for withdrawal view + fix form

--- src/pages/withdrawal.vue
+++ src/pages/withdrawal.vue
@@ -4,9 +4,9 @@ meta:
 </route>
 
 <script setup lang="ts">
-import { BinaryTree } from "~/classe/binary-tree";
-import { BinaryTreeLevel } from "~/classe/binary-tree-level";
-import { probaWithdraw } from "~/utils/withdrawal";
+import { BinaryTree } from '~/classe/binary-tree';
+import type { BinaryTreeLevel } from '~/classe/binary-tree-level';
+import { probaWithdraw } from '~/utils/withdrawal';
 
 const props = defineProps<{
     loading: boolean
@@ -30,80 +30,98 @@ const currentPoint = ref(4);
 const withdrawal = ref(200);
 const dodge = ref(100);
 
-const probas = computed((): { success: number, proba: number }[] => {
-    const tree = new BinaryTree<{ success: number; proba: number, factors: number[] }>(toWithdraw.value);
+const probas = computed((): { success: number; proba: number }[] => {
+    const tree = new BinaryTree<{ success: number; proba: number; factors: number[] }>(toWithdraw.value);
     tree.initTree();
 
-    const level: BinaryTreeLevel<{ success: number; proba: number, factors: number[] }> = tree.getLastLevel();
-    level.nodes.forEach(l => {
+    const level: BinaryTreeLevel<{ success: number; proba: number; factors: number[] }> = tree.getLastLevel();
+    level.nodes.forEach((l) => {
         l.data = {
-            success: l.path.reduce((a,b) => a+b, 0),
+            success: l.path.reduce((a, b) => a + b, 0),
             proba: 1,
             factors: [],
         };
         let success = 0;
-        for (let i=0; i<l.path.length; i++) {
-            let proba = probaWithdraw(maxPoint.value, currentPoint.value-success, withdrawal.value, dodge.value);
-            success+= l.path[i];
+        for (let i = 0; i < l.path.length; i++) {
+            let proba = probaWithdraw(maxPoint.value, currentPoint.value - success, withdrawal.value, dodge.value);
+            success += l.path[i];
             if (l.path[i] === 0) {
-                proba = 1-proba;
+                proba = 1 - proba;
             }
-            l.data?.factors.push(proba*100);
+            l.data?.factors.push(proba * 100);
             l.data.proba *= proba;
         }
     });
-    const res: { success: number, proba: number }[] = [];
+    const res: { success: number; proba: number }[] = [];
 
-    for (let i=0; i<=toWithdraw.value; i++) {
-        let probas = level.nodes.filter(n => n.data?.success === i).map(n => n.data?.proba || 0);
+    for (let i = 0; i <= toWithdraw.value; i++) {
+        const probas = level.nodes.filter(n => n.data?.success === i).map(n => n.data?.proba || 0);
         res.push({
             success: i,
-            proba: probas.reduce((a, b) => a + b, 0)*100,
+            proba: probas.reduce((a, b) => a + b, 0) * 100,
         });
     }
 
     return res;
-})
-
+});
 </script>
 
 <template>
     <Card>
         <template #title>
-            Data
+            {{ t('withdraw.data.title') }}
         </template>
         <template #content>
-            Your target have :
-            <br>
-            <InputNumber v-model="maxPoint" :min="0" :max="100" /> MP/AP max
-            <br>
-            currently <InputNumber v-model="currentPoint" :min="0" :max="100" /> MP/AP
-            <br>
-            <InputNumber v-model="dodge" :min="0" :max="1000" /> dodge
-            <br>
-            <br>
-            <br>
-            You have <InputNumber v-model="withdrawal" :min="0" :max="1000" /> withdrawal
-            <br>
-            and you want to withdraw <InputNumber v-model="toWithdraw" :min="0" :max="1000" /> MP/AP
+            <div class="grid grid-cols-2 gap-3 place-content-center">
+                <div class="flex justify-end">
+                    <div class="text-right align-middle pr-2 pt-1">{{ t('withdraw.data.theTargetHasMax') }}</div>
+                    <InputNumber v-model="maxPoint" class="withdraw-input  p-inputtext-sm" show-buttons button-layout="horizontal" :step="1" :min="0" :max="100" increment-button-icon="pi pi-plus" decrement-button-icon="pi pi-minus" />
+                </div>
+                <div class="flex text-left align-middle pt-1">{{ t('withdraw.data.mpAp') }}</div>
+
+                <div class="flex justify-end">
+                    <div class="text-right align-middle pr-2 pt-1">{{ t('withdraw.data.theTargetHasCurrently') }}</div>
+                    <InputNumber v-model="currentPoint" class="withdraw-input  p-inputtext-sm" show-buttons button-layout="horizontal" :step="1" :min="0" :max="100" increment-button-icon="pi pi-plus" decrement-button-icon="pi pi-minus" />
+                </div>
+                <div class="flex text-left align-middle pt-1">{{ t('withdraw.data.mpAp') }}</div>
+
+                <div class="flex justify-end">
+                    <div class="text-right align-middle pr-2 pt-1">{{ t('withdraw.data.theTargetHas') }}</div>
+                    <InputNumber v-model="dodge" class="withdraw-input  p-inputtext-sm" show-buttons button-layout="horizontal" :step="1" :min="0" :max="1000" increment-button-icon="pi pi-plus" decrement-button-icon="pi pi-minus" />
+                </div>
+                <div class="flex text-left align-middle pt-1">{{ t('withdraw.data.dodge') }}</div>
+            </div>
+
+            <div class="grid grid-cols-2 gap-3 place-content-center mt-10">
+                <div class="flex justify-end">
+                    <div class="text-right align-middle pr-2 pt-1">{{ t('withdraw.data.playerHave') }}</div>
+                    <InputNumber v-model="withdrawal" class="withdraw-input  p-inputtext-sm" show-buttons button-layout="horizontal" :step="1" :min="0" :max="1000" increment-button-icon="pi pi-plus" decrement-button-icon="pi pi-minus" />
+                </div>
+                <div class="flex text-left align-middle pt-1">{{ t('withdraw.data.withdrawal') }}</div>
+
+                <div class="flex justify-end">
+                    <div class="text-right align-middle pr-2 pt-1">{{ t('withdraw.data.wantToWithdraw') }}</div>
+                    <InputNumber v-model="toWithdraw" class="withdraw-input  p-inputtext-sm" show-buttons button-layout="horizontal" :step="1" :min="0" :max="1000" increment-button-icon="pi pi-plus" decrement-button-icon="pi pi-minus" />
+                </div>
+                <div class="flex text-left align-middle pt-1">{{ t('withdraw.data.mpAp') }}</div>
+            </div>
         </template>
     </Card>
 
     <Card class="mt-4">
         <template #title>
-            Result
+            {{ t('withdraw.result.title') }}
         </template>
         <template #content>
-
             <DataTable
                 :value="probas"
             >
-                <Column field="success" header="number of PM/AP withdraw">
+                <Column field="success" :header="t('withdraw.result.numberWithdrew')">
                     <template #body="{ data, field }">
                         {{ data[field] }}
                     </template>
                 </Column>
-                <Column field="proba" header="Chance of event">
+                <Column field="proba" :header="t('withdraw.result.chanceOfEvent')">
                     <template #body="{ data, field }">
                         {{ data[field].toPrecision(4) }}%
                     </template>
@@ -114,4 +132,7 @@ const probas = computed((): { success: number, proba: number }[] => {
 </template>
 
 <style scoped>
+.withdraw-input :deep(.p-inputtext) {
+    width: 64px;
+}
 </style>
